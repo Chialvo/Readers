e30b58120335d26f9bc76e9773a9736a27e25c44
Your Name <lautarochialvo2005@gmail.com>
2026-06-10 21:12:57 -0300

    postgresql

--- Readers/config/settings.py
+++ Readers/config/settings.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 from dotenv import load_dotenv
+import dj_database_url
 import os
 
 load_dotenv()
@@ -56,10 +57,10 @@ TEMPLATES = [
 WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=os.getenv('DATABASE_URL', 'postgresql://lautaro@localhost:5432/readers_db'),
+        conn_max_age=600,
+    )
 }
 
 AUTH_PASSWORD_VALIDATORS = [
